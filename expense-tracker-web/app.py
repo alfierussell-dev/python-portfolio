47bb88311f2d846fd43c04a08a158dc3941f1d08
Alfie Russell <alfiejoerussell@gmail.com>
2026-01-29 18:11:24 +0000

    Added html.

--- expense-tracker-web/app.py
+++ expense-tracker-web/app.py
@@ -1,12 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.get("/")
 def index():
-    return "Hello, Expense Tracker!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    print("Starting server... open http://127.0.0.1:5000")
-    app.run(debug=True, host="127.0.0.1", port=5000)
-
+    app.run(debug=True)
