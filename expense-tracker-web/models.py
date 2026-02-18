2a0449219215d5b4f28c48b326b5fba34e8b970b
Alfie Russell <alfiejoerussell@gmail.com>
2026-02-18 12:26:09 +0000

    added a models element to standardise ui.

---
+++ expense-tracker-web/models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integar, primary_key=True)
+
+    email = db.Column(db.String(255), unique=True, nullable=False, index=True)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    def set_password(self, raw_password: str) -> None:
+        self.password_hash = generate_password_hash(raw_password)
+
+    def  check_password(self, raw_password: str) -> bool:
+        return check_password_hash(self.password_hash, raw_password)
